675e73a8af83a080ca296ad3ebf1b95a81001f63
Andrei <bogachq@mail.ru>
2019-08-22 23:39:10 +0300

    Updates

---
+++ precache-manifest.fb1613640a21fb05d8b043a700d1989c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83e93b0f1044e4229494131aa062ae2",
+    "url": "/TestReact/index.html"
+  },
+  {
+    "revision": "869bc61d10c453533956",
+    "url": "/TestReact/static/css/main.6782cc63.chunk.css"
+  },
+  {
+    "revision": "cc6dd2587c208a529224",
+    "url": "/TestReact/static/js/2.c6a95963.chunk.js"
+  },
+  {
+    "revision": "869bc61d10c453533956",
+    "url": "/TestReact/static/js/main.06037e8e.chunk.js"
+  },
+  {
+    "revision": "8635430c14d8cefebbcf",
+    "url": "/TestReact/static/js/runtime~main.b0875435.js"
+  }
+]);
